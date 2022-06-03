9071a40a69a35bb37befa5de62e2e56ba2263545
David Chuvik <dchuvik@gmail.com>
2022-06-03 11:51:37 -0400

    formatted

--- SyntaxExercise/Program.cs
+++ SyntaxExercise/Program.cs
@@ -6,7 +6,7 @@ namespace SyntaxExercise
     {
         static void Main(string[] args)
         {
-            // GIVEN CODE
+// GIVEN CODE
             //int answer = 4;
             //string response;
             //if (answer < 9)
@@ -18,7 +18,7 @@ namespace SyntaxExercise
             //    response = answer + " is greater than or equal to nine";
             //}
 
-            // WORKING CODE
+// WORKING CODE
             //var answer = 4;
             //string response;
             //if (answer < 9)
@@ -31,7 +31,7 @@ namespace SyntaxExercise
             //}
             //Console.WriteLine(response);
 
-            // EXERCISE COMPLETED CODE
+// EXERCISE COMPLETED CODE
             Console.WriteLine("Enter a number: ");
             var answer = Convert.ToInt32(Console.ReadLine());
             string response = (answer < 9) ? $"{answer} is less than nine" : $"{answer} is greater than or equal to nine";
